fda8d4028153e0a6a8acf20414167c3be354e36c
Anupam Gorthi <1ne@users.noreply.github.com>
2016-08-17 20:39:58 -0400

    Update fish-install.sh

--- Scripts/fish-install.sh
+++ Scripts/fish-install.sh
@@ -22,6 +22,16 @@ brew update
 brew install coreutils binutils fish zsh htop homebrew/dupes/nano
 brew cleanup
 
+##### Compiling MTR from git HEAD and installing it #####
+git clone https://github.com/traviscross/mtr.git
+cd mtr
+./bootstrap.sh
+./configure --without-gtk
+make
+sudo make install
+cd .. 
+rm -rf mtr/
+
 ##### Adding fish and ZSH shells to shells #####
 echo $(which fish) | sudo tee -a /etc/shells
 echo $(which zsh) | sudo tee -a /etc/shells
@@ -38,226 +48,6 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-
-
-##### Creating zpreztorc file #####
-bash
-echo '
-#
-# Sets Prezto options.
-#
-# Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
-#
-
-#
-# General
-#
-
-# Set case-sensitivity for completion, history lookup, etc.
-# zstyle ':prezto:*:*' case-sensitive 'yes'
-
-# Color output (auto set to 'no' on dumb terminals).
-zstyle ':prezto:*:*' color 'yes'
-
-# Set the Zsh modules to load (man zshmodules).
-# zstyle ':prezto:load' zmodule 'attr' 'stat'
-
-# Set the Zsh functions to load (man zshcontrib).
-# zstyle ':prezto:load' zfunction 'zargs' 'zmv'
-
-# Set the Prezto modules to load (browse modules).
-# The order matters.
-zstyle ':prezto:load' pmodule \
-  'environment' \
-  'terminal' \
-  'editor' \
-  'history' \
-  'directory' \
-  'spectrum' \
-  'utility' \
-  'ssh' \
-  'completion' \
-  'homebrew' \
-  'osx' \
-  'ruby' \
-  'rails' \
-  'git' \
-  'python' \
-  'rsync' \
-  'spectrum' \
-  'screen' \
-  'gpg' \
-  'gnu-utility' \
-  'utility' \
-  'syntax-highlighting' \
-  'history-substring-search' \
-  'prompt' \
-
-#
-# Editor
-#
-
-# Set the key mapping style to 'emacs' or 'vi'.
-zstyle ':prezto:module:editor' key-bindings 'emacs'
-
-# Auto convert .... to ../..
-# zstyle ':prezto:module:editor' dot-expansion 'yes'
-
-#
-# Git
-#
-
-# Ignore submodules when they are 'dirty', 'untracked', 'all', or 'none'.
-# zstyle ':prezto:module:git:status:ignore' submodules 'all'
-
-#
-# GNU Utility
-#
-
-# Set the command prefix on non-GNU systems.
-# zstyle ':prezto:module:gnu-utility' prefix 'g'
-
-#
-# History Substring Search
-#
-
-# Set the query found color.
-# zstyle ':prezto:module:history-substring-search:color' found ''
-
-# Set the query not found color.
-# zstyle ':prezto:module:history-substring-search:color' not-found ''
-
-# Set the search globbing flags.
-# zstyle ':prezto:module:history-substring-search' globbing-flags ''
-
-#
-# Pacman
-#
-
-# Set the Pacman frontend.
-# zstyle ':prezto:module:pacman' frontend 'yaourt'
-
-#
-# Prompt
-#
-
-# Set the prompt theme to load.
-# Setting it to 'random' loads a random theme.
-# Auto set to 'off' on dumb terminals.
-zstyle ':prezto:module:prompt' theme 'sorin'
-
-#
-# Ruby
-#
-
-# Auto switch the Ruby version on directory change.
-# zstyle ':prezto:module:ruby:chruby' auto-switch 'yes'
-
-#
-# Screen
-#
-
-# Auto start a session when Zsh is launched in a local terminal.
-# zstyle ':prezto:module:screen:auto-start' local 'yes'
-
-# Auto start a session when Zsh is launched in a SSH connection.
-# zstyle ':prezto:module:screen:auto-start' remote 'yes'
-
-#
-# SSH
-#
-
-# Set the SSH identities to load into the agent.
-# zstyle ':prezto:module:ssh:load' identities 'id_rsa' 'id_rsa2' 'id_github'
-
-#
-# Syntax Highlighting
-#
-
-# Set syntax highlighters.
-# By default, only the main highlighter is enabled.
-# zstyle ':prezto:module:syntax-highlighting' highlighters \
-#   'main' \
-#   'brackets' \
-#   'pattern' \
-#   'cursor' \
-#   'root'
-#
-# Set syntax highlighting styles.
-# zstyle ':prezto:module:syntax-highlighting' styles \
-#   'builtin' 'bg=blue' \
-#   'command' 'bg=blue' \
-#   'function' 'bg=blue'
-
-#
-# Terminal
-#
-
-# Auto set the tab and window titles.
-# zstyle ':prezto:module:terminal' auto-title 'yes'
-
-# Set the window title format.
-# zstyle ':prezto:module:terminal:window-title' format '%n@%m: %s'
-
-# Set the tab title format.
-# zstyle ':prezto:module:terminal:tab-title' format '%m: %s'
-
-#
-# Tmux
-#
-
-# Auto start a session when Zsh is launched in a local terminal.
-# zstyle ':prezto:module:tmux:auto-start' local 'yes'
-
-# Auto start a session when Zsh is launched in a SSH connection.
-# zstyle ':prezto:module:tmux:auto-start' remote 'yes'
-
-# Integrate with iTerm2.
-# zstyle ':prezto:module:tmux:iterm' integrate 'yes'
-' > ~/.zpreztorc
-
-##### Creating zshrc file #####
-echo '
-#
-# Executes commands at the start of an interactive session.
-#
-# Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
-#
-
-# Source Prezto.
-if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
-fi
-
-# Customize to your needs...
-#source /usr/local/bin/aws_zsh_completer.sh
-
-# Prezto update
-alias pzupdate='cd ~/.zprezto && git pull && git submodule update --init --recursive && cd -'
-
-PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-
-alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
-alias ls='ls --color=auto'
-
-source /usr/local/share/zsh/site-functions/_aws
-
-# Android
-export ANDROID_HOME=/usr/local/opt/android-sdk
-#/usr/local/etc/bash_completion.d
-
-eval "$(thefuck --alias)"
-
-export PATH=$PATH:/usr/local/opt/go/libexec/bin
-export GOPATH=$HOME/work/go
-export PATH=$PATH:$GOPATH/bin
-
-alias brewski='brew update && brew upgrade --all && brew cask list | xargs brew cleanup; brew cask cleanup; brew doctor'
-alias pipu='pip freeze --local | grep -v \'\^\-\e' | cut -d = -f 1  | xargs -n1 pip install -U'
-' > ~/.zshrc
 
 ##### ZSH Completions #####
 brew install zsh-autosuggestions zsh-history-substring-search zsh-navigation-tools zshdb zsh-completions zsh-lovers zsh-syntax-highlighting
@@ -282,16 +72,6 @@ zsh
 
 ##### Incase glibc install fails #####
 #/bin/rm -rf ~/.linuxbrew/Cellar/glibc
-
-##### Compiling MTR from git HEAD and installing it #####
-git clone https://github.com/traviscross/mtr.git
-cd mtr
-./bootstrap.sh
-./configure --without-gtk
-make
-sudo make install
-cd .. 
-rm -rf mtr/
 
 ##### Changing to zsh shell #####
 sudo chsh -s $(which zsh) $USER
