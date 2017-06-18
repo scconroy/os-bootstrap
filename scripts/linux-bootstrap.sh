d573d75b0b449083e28a8180cb499ff529a61328
Anupam Gorthi <1ne@users.noreply.github.com>
2017-06-17 22:19:16 -0400

    Create linux-bootstrap.sh

---
+++ scripts/linux-bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum groupinstall -y 'Development Tools' && sudo yum install -y curl file git irb python-setuptools ruby mlocate
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"' >>~/.bash_profile
+source ~/.bash_profile
+
+#sudo visudo
+#Defaults    secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin
+
+brew tap linuxbrew/extra
+brew install libpcap
+
+brew install bash fish zsh zsh-autosuggestions zsh-completions zshdb zsh-history-substring-search zsh-lovers zsh-navigation-tools zsh-syntax-highlighting
+
+sudo echo '/home/linuxbrew/.linuxbrew/bin/bash'>> /etc/shells
+sudo echo '/home/linuxbrew/.linuxbrew/bin/zsh'>> /etc/shells
+sudo echo '/home/linuxbrew/.linuxbrew/bin/fish'>> /etc/shells
+
+#chsh -s /usr/local/bin/bash $USER
+sudo chsh -s /home/linuxbrew/.linuxbrew/bin/zsh $USER
+#chsh -s /usr/local/bin/fish $USER
+chmod go-w '/home/linuxbrew/.linuxbrew/share'
+
+##### Adding nanorc to config #####
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+
+##### Installing prezto #####
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+cat << EOF
+zsh | q
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+wget https://raw.githubusercontent.com/1ne/bootstrap/master/conf/linux/.zshrc -O ~/.zshrc
+wget https://raw.githubusercontent.com/1ne/bootstrap/master/conf/.zpreztorc -O ~/.zpreztorc
+
+sudo visudo
+Defaults    secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin
+
+EOF
