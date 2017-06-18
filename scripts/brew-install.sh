104f9e7998fb346da2d578788479091eeb5f5ef3
Anupam Gorthi <1ne@users.noreply.github.com>
2017-06-17 22:20:40 -0400

    Create brew-install.sh

---
+++ scripts/brew-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+brew install awscli aws-shell binutils bzip2 coreutils curl wget findutils gawk git go jq libbsd libssh2 mysql
+brew install nano nmap openssh openssl pv python python3 redis ruby sshrc strace unzip whois xz
+brew install bsdmainutils fping htop iftop iotop ioping procps sysstat tcpdump tcpstat twoping netcat ncdu
+
+brew install arping dhcping dns2tcp dnsmap dnsperf dnstracer fio iperf3 liboping mtr stress sysbench tcptraceroute
+
+brew reinstall curl --with-c-ares  --with-libmetalink --with-libssh2 --with-nghttp2 --with-rtmpdump
+
+sudo rpm -ivh https://www.atoptool.nl/download/atop-2.3.0-1.el6.x86_64.rpm
+go get -u github.com/rakyll/hey
+pip install beeswithmachineguns
