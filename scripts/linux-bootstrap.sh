ac6058a8e3e22618cbd5acd3347fdcfcd1fefe86
Anupam Gorthi <1ne@users.noreply.github.com>
2017-06-22 23:25:00 -0400

    Update linux-bootstrap.sh

--- scripts/linux-bootstrap.sh
+++ scripts/linux-bootstrap.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+base_path="https://raw.githubusercontent.com/1ne/bootstrap/master"
+
+##### Updating the System #####
 sudo yum update -y
 sudo yum groupinstall -y 'Development Tools' && sudo yum install -y curl file git irb python-setuptools ruby mlocate golang awslogs
 
+##### Installing LinuxBrew #####
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"' >>~/.bash_profile
 source ~/.bash_profile
+chmod go-w '/home/linuxbrew/.linuxbrew/share'
 
+##### Tapping Brew Extras #####
 brew tap linuxbrew/extra
 brew install libpcap
 
+##### Installing the Shells and Plugins #####
 brew install bash fish zsh zsh-autosuggestions zsh-completions zshdb zsh-history-substring-search zsh-lovers zsh-navigation-tools zsh-syntax-highlighting
 
+##### Adding Shells to list #####
 sudo echo '/home/linuxbrew/.linuxbrew/bin/bash'>> /etc/shells
 sudo echo '/home/linuxbrew/.linuxbrew/bin/zsh'>> /etc/shells
 sudo echo '/home/linuxbrew/.linuxbrew/bin/fish'>> /etc/shells
 
+##### Chainging User Shells #####
 #chsh -s /usr/local/bin/bash $USER
 sudo chsh -s /home/linuxbrew/.linuxbrew/bin/zsh $USER
 #chsh -s /usr/local/bin/fish $USER
-chmod go-w '/home/linuxbrew/.linuxbrew/share'
 
 ##### Adding nanorc to config #####
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
@@ -29,15 +37,15 @@ curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 touch ~/.zshrc
 /home/linuxbrew/.linuxbrew/bin/zsh -i -c 'setopt EXTENDED_GLOB && for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"; done'
-wget https://raw.githubusercontent.com/1ne/bootstrap/master/conf/linux/.zshrc -q -O ~/.zshrc
-wget https://raw.githubusercontent.com/1ne/bootstrap/master/conf/.zpreztorc -q -O ~/.zpreztorc
+wget $base_path/conf/linux/.zshrc -q -O ~/.zshrc
+wget $base_path/conf/.zpreztorc -q -O ~/.zpreztorc
 
 ##### Downloading Custom Stuff #####
-wget https://raw.githubusercontent.com/1ne/bootstrap/master/scripts/curl-format -q -O ~/curl-format
-
-wget https://raw.githubusercontent.com/1ne/bootstrap/master/scripts/brew-install.sh -q
+wget $base_path/curl-format -q -O ~/curl-format
+wget $base_path/brew-install.sh -q
 chmod +x brew-install.sh
 
+##### Print Additonal ToDo Stuff #####
 cat << EOF
 ####################################################
 Edit the sudoers file and replace the line.
