4dab3ccd7968184f8c2c14b5dd692a6ad9c5d65e
Anupam Gorthi <1ne@users.noreply.github.com>
2017-06-27 00:14:57 -0400

    Update clone-instance.sh

--- assets/clone-instance.sh
+++ assets/clone-instance.sh
@@ -19,7 +19,6 @@ region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/documen
 #ami_id="ami-643b1972"
 
 #### Get Latest Amazon Linux AMI ID ####
-#ami_id=(aws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn-ami-hvm-????.??.?.x86_64-gp2' 'Name=state,Values=available' | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
 #ami_id=(aws ec2 describe-images --owners amazon --filters \
 #    'Name=name,Values=amzn-ami-hvm-????.??.?.????????-x86_64-gp2' \
 #    'Name=state,Values=available' \
