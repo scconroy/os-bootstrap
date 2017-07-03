8d6605da3bd564f76543520224bea5d533219019
Anupam Gorthi <1ne@users.noreply.github.com>
2017-07-03 15:57:49 -0400

    Update brew-install.sh

--- scripts/brew-install.sh
+++ scripts/brew-install.sh
@@ -3,7 +3,7 @@
 brew install awscli aws-shell binutils bzip2 coreutils curl wget findutils gawk git ipbt ipv6calc ipv6toolkit ip_relay jq libbsd libssh2 mysql valgrind
 brew install nano nmap openssh openssl pv python python3 pip-completion brew-pip redis rsync ruby s3cmd screen sshrc strace unzip whois xz 
 
-brew install bsdmainutils fping htop iftop iotop ioping procps sysstat tcpdump tcpstat twoping netcat nethogs ncdu
+brew install bsdmainutils fping htop iftop iotop ioping procps sysstat tcpdump tcpstat twoping netcat nethogs ncdu ifstat httpstat
 brew install arping dhcping dns2tcp dnsmap dnsperf dnstracer fio iperf3 liboping mtr stress sysbench tcptraceroute
 
 brew reinstall curl --with-c-ares  --with-libmetalink --with-libssh2 --with-nghttp2 --with-rtmpdump
