dcf21bf484cbc3afc27e99381f10670fc817c5d2
Anupam Gorthi <1ne@users.noreply.github.com>
2017-07-17 20:35:23 -0400

    Update brew-install.sh

--- scripts/brew-install.sh
+++ scripts/brew-install.sh
@@ -6,20 +6,21 @@ base_path="https://raw.githubusercontent.com/1ne/os-bootstrap/master"
 ##### Configuring ZSH Reverse Search #####
 sed -i -e "s/local border=0/local border=1/g" ~/.config/znt/n-list.conf
 
-##### Installing Utilities ####
+########## Installing Utilities #########
 
 ##### Installing OS Utilities ####
 brew install htop procps sysstat
 brew install stress sysbench
 brew install awscli aws-shell binutils coreutils curl wget bsdmainutils bzip2 findutils gawk git ipbt jq libbsd libssh2 mysql valgrind
-brew install nano nmap openssh openssl pv python python3 pip-completion brew-pip redis rsync ruby s3cmd screen sshrc strace unzip xz
+brew install nano openssh openssl pv python python3 pip-completion brew-pip redis rsync ruby s3cmd screen sshrc strace unzip xz
 
 ##### Installing Disk Utilities ####
 brew install iotop ioping ncdu fio
 
 ##### Installing Network Utilities ####
-brew install whois fping iftop tcpdump tcpstat twoping netcat nethogs ifstat httpstat ipv6calc ipv6toolkit ip_relay
-brew install arping dhcping dns2tcp dnsmap dnsperf dnstracer iperf3 liboping mtr tcptraceroute dnstop jnettop
+brew install iftop tcpdump tcpstat nethogs ifstat dnstop jnettop mtr tcptraceroute netcat nmap iperf3
+brew install arping fping liboping twoping httpstat ipv6calc ipv6toolkit ip_relay
+brew install whois dns2tcp dnsmap dnsperf dnstracer dhcping  
 
 ##### Installing cURL with HTTP/2 Support ####
 brew reinstall curl --with-c-ares  --with-libmetalink --with-libssh2 --with-nghttp2 --with-rtmpdump
