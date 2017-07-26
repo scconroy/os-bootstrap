928714eea58ccfdb675f3182b673032bda529d1a
Anupam Gorthi <1ne@users.noreply.github.com>
2017-07-25 20:14:21 -0400

    Update linux-bootstrap-rhel7.sh

--- scripts/linux-bootstrap-rhel7.sh
+++ scripts/linux-bootstrap-rhel7.sh
@@ -39,9 +39,10 @@ sudo service awslogs start
 sudo service awslogs status
 
 ##### Installing SSM Agent #####
+mkdir /tmp/ssm
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-sudo start amazon-ssm-agent
-sudo status amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
 
 ##### Installing LinuxBrew #####
 echo | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
