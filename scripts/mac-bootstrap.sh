a320de129f64c60fff31538817da8396c3544595
Anupam Gorthi <1ne@users.noreply.github.com>
2017-10-09 15:12:27 -0400

    Update mac-bootstrap.sh

--- scripts/mac-bootstrap.sh
+++ scripts/mac-bootstrap.sh
@@ -13,7 +13,8 @@ base_path="https://raw.githubusercontent.com/1ne/os-bootstrap/master"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ##### Installing the Shells and Plugins #####
-brew install bash fish zsh zsh-autosuggestions zsh-completions zshdb zsh-history-substring-search zsh-lovers zsh-navigation-tools zsh-syntax-highlighting
+brew install git wget
+brew install bash fish zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-lovers zsh-navigation-tools zsh-syntax-highlighting 
 
 ##### Adding Shells to list #####
 echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
@@ -21,7 +22,7 @@ echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
 
 ##### Adding nanorc to config #####
-curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+#curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
 ##### Installing prezto #####
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -31,8 +32,8 @@ wget $base_path/conf/mac/zshrc -q -O ~/.zshrc
 wget $base_path/conf/zpreztorc -q -O ~/.zpreztorc
 
 ##### Downloading the next Script #####
-wget $base_path/scripts/brew-install.sh -q
-chmod +x brew-install.sh
+#wget $base_path/scripts/brew-install.sh -q
+#chmod +x brew-install.sh
 
 ##### Downloading Custom Utils #####
 wget $base_path/assets/curl-format -q -O ~/curl-format
@@ -44,9 +45,9 @@ sudo wget $base_path/assets/ciphers-test -q -O /usr/local/bin/ciphers-test
 sudo chmod 777 /usr/local/bin/ciphers-test
 
 ##### Print Additonal ToDo Stuff #####
-cat << EOF
+#cat << EOF
 ####################################################
-The instance will reboot and kick you out. Please login back and run the following command
-time ./brew-install.sh
+#The instance will reboot and kick you out. Please login back and run the following command
+#time ./brew-install.sh
 ####################################################
-EOF
+#EOF
