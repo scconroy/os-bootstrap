096f9217f2f2974309c06e7e7bfa97a304f8f204
Anupam Gorthi <1ne@users.noreply.github.com>
2017-10-09 15:38:42 -0400

    Update brew-install-mac.sh

--- scripts/brew-install-mac.sh
+++ scripts/brew-install-mac.sh
@@ -25,35 +25,23 @@ brew install libpcap
 
 ##### Installing AWS Utilities ####
 brew install python python3 ruby pip-completion brew-pip 
-brew install awscli aws-shell awsebcli awslogs s3cmd
+sudo pip3 install --upgrade awscli aws-shell awsebcli awslogs s3cmd
 
 ##### Configuring AWS CLI Config #####
 mkdir ~/.aws
 wget $base_path/assets/aws-config -q -O ~/.aws/config
 
 ##### Installing OS Utilities ####
-brew install htop procps sysstat 
-brew install stress sysbench
-brew install binutils coreutils strace valgrind curl wget gawk git nano jq findutils ddate bsdmainutils libbsd pv peco
-brew install openssh libssh2 sshrc openssl rsync screen ipbt unzip bzip2 xz ddar
-brew install redis
-
-##### Installing Disk Utilities ####
-brew install iotop ioping ncdu fio dc3dd ddrescue
+brew install htop procps
+brew install binutils coreutils valgrind curl gawk nano jq findutils ddate pv peco
+brew install openssh libssh2 sshrc openssl openssh rsync screen unzip bzip2 xz ddar p7zip
+brew cask install keka
 
 ##### Installing Network Utilities ####
-brew install iftop tcpdump tcpstat nethogs ifstat dnstop jnettop mtr tcptraceroute netcat nmap iperf3
-brew install arping fping liboping twoping httpstat ipv6calc ipv6toolkit ip_relay
-brew install whois dns2tcp dnsmap dnsperf dnstracer dhcping
+brew install tcpdump tcpstat jnettop mtr tcptraceroute netcat nmap iperf3 whois arping fping liboping httpstat ipv6calc
 
 ##### Installing cURL with HTTP/2 Support ####
 brew reinstall curl --with-c-ares  --with-libmetalink --with-libssh2 --with-nghttp2 --with-rtmpdump
 
 ##### Installing Monitoring Tools #####
-pip install glances
-
-##### Installing Web-Benchmarking Tools #####
-pip install six bottle
-pip install beeswithmachineguns
-go get -u github.com/rakyll/hey
-go get -u github.com/tsenart/vegeta
+pip3 install glances
