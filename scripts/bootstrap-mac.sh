c9392ef206a3a8d6243d67e63296c4c91d9d7d0c
Anupam Gorthi <1ne@users.noreply.github.com>
2017-11-01 13:59:45 -0400

    Update bootstrap-mac.sh

--- scripts/bootstrap-mac.sh
+++ scripts/bootstrap-mac.sh
@@ -13,7 +13,7 @@ base_path="https://raw.githubusercontent.com/1ne/os-bootstrap/master"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ##### Installing the Shells and Plugins #####
-brew install git wget axel curl
+brew install git wget axel curl coreutils binutils
 brew cask install iterm2 google-chrome keka flycut
 brew install bash fish zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-lovers zsh-navigation-tools zsh-syntax-highlighting 
 
