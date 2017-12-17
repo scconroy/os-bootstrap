f5e29fd5f0dd71b77ed7506fc16889afaa31074f
Anupam Gorthi <1ne@users.noreply.github.com>
2017-12-17 18:55:48 -0500

    Update brew-install-amzn2.sh

--- scripts/brew-install-amzn2.sh
+++ scripts/brew-install-amzn2.sh
@@ -62,20 +62,6 @@ brew install whois dns2tcp dnsmap dnsperf dnstracer dhcping
 ##### Installing cURL with HTTP/2 Support ####
 brew reinstall curl --with-c-ares  --with-libmetalink --with-libssh2 --with-nghttp2 --with-rtmpdump
 
-##### Installing Monitoring Tools #####
-pip install glances
-sudo rpm -ivh https://www.atoptool.nl/download/atop-2.3.0-1.el7.x86_64.rpm
-sudo systemctl enable atop
-sudo systemctl start  atop
-sudo systemctl status atop
-
-##### Installing Sysdig Monitoring Tools #####
-sudo rpm --import https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public
-sudo curl -s -o /etc/yum.repos.d/draios.repo http://download.draios.com/stable/rpm/draios.repo
-sudo rpm -i https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum -y install kernel-devel-$(uname -r)
-sudo yum -y install sysdig
-
 ##### Installing Web-Benchmarking Tools #####
 pip install six bottle
 pip install beeswithmachineguns
