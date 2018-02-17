657368d5f292a65a8e2eeeaa5d4a9cafc9cd73d0
Anupam Gorthi <1ne@users.noreply.github.com>
2018-02-17 00:53:11 -0500

    Create chime-cleanse.sh

---
+++ assets/chime-cleanse.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ~/Library/Application\ Support/com.amazon.Amazon-Chime                                                                               ✘ 1
+rm -rf ~/Library/WebKit/com.amazon.Amazon-Chime
+rm -rf ~/Library/Caches/com.plausiblelabs.crashreporter.data
+rm -rf ~/Library/Caches/com.amazon.Amazon-Chime/
