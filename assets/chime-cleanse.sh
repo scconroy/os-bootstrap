9d9d3e3195c2e855a287041ce5e5a00b2f51635d
Anupam Gorthi <1ne@users.noreply.github.com>
2018-02-18 23:38:16 -0500

    Update chime-cleanse.sh

--- assets/chime-cleanse.sh
+++ assets/chime-cleanse.sh
@@ -6,5 +6,5 @@ rm -rf ~/Library/Caches/com.plausiblelabs.crashreporter.data/
 rm -rf ~/Library/Cookies/com.amazon.Amazon-Chime.binarycookies
 rm -rf ~/Library/Logs/Amazon\ Chime/
 rm -rf ~/Library/Preferences/com.amazon.Amazon-Chime.plist
-rm -rf ~/Library/Saved Application\ State/com.amazon.Amazon-Chime.savedState
+rm -rf ~/Library/Saved\ Application\ State/com.amazon.Amazon-Chime.savedState
 rm -rf ~/Library/WebKit/com.amazon.Amazon-Chime
