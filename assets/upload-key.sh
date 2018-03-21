3beded43b998f6cfc5509cf87f72a05ad25f9ab3
Anupam Gorthi <1ne@users.noreply.github.com>
2018-03-21 17:56:11 -0400

    Update upload-key.sh

--- assets/upload-key.sh
+++ assets/upload-key.sh
@@ -2,5 +2,5 @@
 regions=$(aws ec2 describe-regions --output text | cut -f 3)
 while read line
 do
-    aws ec2 import-key-pair --key-name $2 --public-key-material "$(cat $2)"
+    aws ec2 import-key-pair --key-name $1 --public-key-material "$(cat $2)"
 done <<< "$regions"
