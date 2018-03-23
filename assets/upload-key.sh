bc2303cc49c944b1a639b0b2407f6e27ab0e5b58
Anupam Gorthi <anupam.gorthi@gmail.com>
2018-03-23 00:14:59 -0400

    Fix upload-key.sh

--- assets/upload-key.sh
+++ assets/upload-key.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+$1="${$1:-~/.ssh/authorized_keys}"
 regions=$(aws ec2 describe-regions --output text | cut -f 3)
 while read line
 do
-    aws ec2 import-key-pair --key-name $1 --public-key-material "$(cat $2)"
+    aws ec2 import-key-pair --key-name "$(cat $1 | cut -f3 -d' ')" --public-key-material "$(cat $1 | cut -f2 -d' ')" --region $line
 done <<< "$regions"
