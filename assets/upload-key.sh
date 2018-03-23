7fab05a1e25b01a1637c8657f19604df8a5d47a2
Anupam Gorthi <anupam.gorthi@gmail.com>
2018-03-23 01:25:59 -0400

    Fix upload-key.sh

--- assets/upload-key.sh
+++ assets/upload-key.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-$1="${$1:-~/.ssh/authorized_keys}"
+
+### Generate PublicKey (if required) ###
+# ssh-keygen -t rsa -b 4096 -C "gorthia" -f .ssh/authorized_keys
+
+pub_key_path=$1
+if [ -z "$1" ]; then
+    pub_key_path="$HOME/.ssh/authorized_keys"
+fi
+
+key_name=$2
+if [ -z "$2" ]; then
+    key_name="$(cat $pub_key_path | cut -f3 -d' ')"
+fi
+
+### Enumerate AWS Regions ###
 regions=$(aws ec2 describe-regions --output text | cut -f 3)
+
 while read line
 do
-    aws ec2 import-key-pair --key-name "$(cat $1 | cut -f3 -d' ')" --public-key-material "$(cat $1 | cut -f2 -d' ')" --region $line
+    echo "For region: $line"
+    aws ec2 import-key-pair --key-name $key_name --public-key-material file://$pub_key_path --region $line
 done <<< "$regions"
