d801b023933e8766589f4d47a5bde864e3ad3200
Anupam Gorthi <anupam.gorthi@gmail.com>
2018-03-25 01:55:29 -0400

    Fix ubuntu sysdig repo issue

--- scripts/ubuntu/ubuntu-sysdig-install.sh
+++ scripts/ubuntu/ubuntu-sysdig-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##### Installing Sysdig Monitoring Tools #####
-curl -s https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public | apt-key add -  
+curl -s https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public | sudo apt-key add -  
 curl -s -o /etc/apt/sources.list.d/draios.list https://s3.amazonaws.com/download.draios.com/stable/deb/draios.list  
 sudo apt update
 sudo apt -y install linux-headers-$(uname -r)
